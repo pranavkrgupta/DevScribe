3abe7ea21d05bfa842779bb044ffa130a2b214b9
Pranav Kumar Gupta <pranavkumar2998@gmail.com>
2025-07-27 00:51:28 +0530

    BaseEntity class added - for common fields which will be used by child entity classes

---
+++ server/src/main/java/com/pranav/devscribe/entities/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.pranav.devscribe.entities;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseEntity {
+
+	@Id
+	@Column(name= "Id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@CreationTimestamp
+	@Column(name = "Creation_Date")
+	private LocalDate creationDate;
+	
+	@UpdateTimestamp
+	@Column(name = "Updated_On")
+	private LocalDateTime updatedOn;
+}
