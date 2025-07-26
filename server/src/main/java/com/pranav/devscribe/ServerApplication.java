e0a349ac95638eeebf7305b5a9dead67631aae68
Pranav Kumar Gupta <pranavkumar2998@gmail.com>
2025-07-27 00:45:56 +0530

    Backend - Initial Commit

---
+++ server/src/main/java/com/pranav/devscribe/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.pranav.devscribe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
